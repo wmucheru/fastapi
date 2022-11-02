41fdf5b2414795283dcc438ce761c094ef646d7f
William Mucheru <willyk99@gmail.com>
2022-11-02 22:21:32 +0300

    Use fastapi_versioning

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from fastapi_versioning import VersionedFastAPI, version
 
-app = FastAPI()
+app = FastAPI(title="Test App")
 
 
-@app.get("/home")
 @app.get("/")
+@version(1)
 async def root():
     return {
         "message": "Hello world"
     }
+
+app = VersionedFastAPI(app,
+                       version_format='{major}',
+                       prefix_format='/v{major}')
